7318f251362e66d29ff9e0ee2c3ed964edc5b443
David V <d16124563@mydit.ie>
2019-02-28 20:48:07 +0000

    Update the Code with SSH connection
    VM creation OK
    SSH OK
    Need to work the rest of the code once connected to the server

--- createVm.sh
+++ createVm.sh
@@ -7,20 +7,38 @@
  az group create --name CloudComputingLab2 --location westeurope
  wait
  echo "Creating the VM"
- az vm create --resource-group CloudComputingLab2 \
+ ip_return=$(echo $(az vm create --resource-group CloudComputingLab2 \
   --name CloudComputingLab2VM \
   --image UbuntuLTS \
   --generate-ssh-keys \
   --output json \
-  --verbose
+  --verbose | awk '/publicIpAddress/ {match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}'))
  wait
  echo "You are done (actually no lol)"
- 
+
+
+echo "SSH connection " $ip_return
+ssh $ip_return
+
+# GOOD until HERE then it goes on the server but the rest of the command are not executed until you leave the server.
+
+# echo "Preparation of the Server"
+# sudo apt-get update
+# sudo apt-get install python3.6
+
+# echo "Download Github file"
+# git clone https://github.com/crakage/DIT3CloudComputingLab2.git
+
+# echo "Initiate Virtual Environement and Libaries"
+# python3 -m venv venv
+# . venv/bin/activate
+# pip install --upgrade pip
+# pip install flask
+# pip install psutil
+
+# echo "Starting Flask Application"
+# python3 main.py 
 
  # Cleanup >> az group delete --name CloudComputingLab2 --no-wait
 
  
- # TODO
- # Get IP address / SSH login /  create virtualenv|install flask & psutil
- # pull the server from github
- # start flask application

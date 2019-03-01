1b9712991f0e871f705dc41d564010909c9a4d13
David V <d16124563@mydit.ie>
2019-03-01 15:38:30 +0000

    Add test file maintest.py

---
+++ maintest.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+from psutil import virtual_memory
+import socket
+import multiprocessing
+
+
+app = Flask(__name__)
+
+
+def get_ip():  # found in stackoverflow.com
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
+# def get_memory():
+#     my_memory = virtual_memory()
+#     return my_memory.total
+
+
+@app.route('/status')
+def info():
+    a = {
+        "hostname": socket.gethostname(),
+        "ip address": get_ip(),
+        "cpus": multiprocessing.cpu_count(),
+        "memory": "get_memory()"
+    }
+    return jsonify(a)
+
+
+app.run(port=8080)
